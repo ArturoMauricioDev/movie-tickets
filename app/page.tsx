63958a4ce987363ea3b5a9ae7543e4f75919a924
Arturo <atercerosb@univalle.edu>
2024-07-11 09:27:31 -0400

    dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/Toggler";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
+      <ModeToggle />
       <h1>este es el home</h1>
+      <Button>Enviar</Button>
     </main>
   );
 }
